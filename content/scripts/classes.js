5d5a1bd4b1db8e30c0152d4f1fd52710c88a586d
zukaeska <esakiazuka@gmail.com>
2021-07-05 10:54:35 +0400

    Update classes.js
    berisha update

--- content/scripts/classes.js
+++ content/scripts/classes.js
@@ -1,12 +1,27 @@
 const Black = "black"; White = "white"; Green = "green"; Brown = "brown"; Yellow = "yellow";
-const Diagonal = "diagonal"; Horizontal = "horizontal"; Both = "both"; king = "king"; queen = "queen"; bishop = "bishop"; rook = "rook";
+const Diagonal = "diagonal"; Horizontal = "horizontal"; Both = "both"; pieceTypeKing = "king"; pieceTypeQueen = "queen"; pieceTypeBishop = "bishop"; pieceTypeRook = "rook";
 const KnightMoves = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]; 
 const Id = "show";
+const Br = "<br />";
+const PossibleMove = "posible moves";
+const Underscore = "_";
+const Location = "./content/images/";
+const Img = "img";
+const Png = ".png";
+const Body = "body";
+const Enter = "#enter";
+const PieceId = "#piece";
+const Comma = ",";
 var previousPiece;
 var rowIndex;
 var columnIndex;
+
 function pawnType (color){
-    if (color == White) return  moves = [1, 2, 2];else  return moves = [-1, -2, 7];
+    if (color == White){
+        return  moves = [1, 2, 2];
+    }else  {
+        return moves = [-1, -2, 7];
+    }
 }
 
 function getNumber(letter){
@@ -14,11 +29,11 @@ function getNumber(letter){
 }
 
 function getName(color, name){
-    return color + "_" + name;
+    return color + Underscore + name;
 }
 
 function coordinate(letter, number){
-    return letter + "_" + number;
+    return letter + Underscore + number;
 }
 
 function getLetter(number){
@@ -27,24 +42,24 @@ function getLetter(number){
 
  //cheks piece type
 function pieceType (name){
-    if (name == king){
+    if (name == pieceTypeKing){
         return Both
     };
-    if (name == rook){
+    if (name == pieceTypeRook){
         return Horizontal
     };
-    if (name == bishop){
+    if (name == pieceTypeBishop){
         return Diagonal
     };
-    if (name == queen){
+    if (name == pieceTypeQueen){
         return Both
     };
 }
 
  //changes background color
 function changeColor (first, second, color) {
-    if (document.getElementById(first + "_" + second) !== null)
-    document.getElementById(first + "_" + second).style.backgroundColor = color ;
+    if (document.getElementById(first + Underscore + second) !== null)
+    document.getElementById(first + Underscore + second).style.backgroundColor = color ;
 }
 
 var brownCell = document.getElementsByClassName(Brown);
@@ -62,59 +77,91 @@ var insert = document.getElementById(Id);
 function posibleMoves(name, type, columnCoordinate, rowCoordinate, color, letter){  
     reset();
     changeColor(getLetter(columnCoordinate), rowCoordinate, Yellow)
-    insert.innerHTML = getName(color, name) + "<br />" + "posible moves" + "<br />";
-    var place = letter + "_" + rowCoordinate;
+    insert.innerHTML = getName(color, name) + Br + PossibleMove + Br;
+    var place = letter + Underscore + rowCoordinate;
     var startPiece = document.getElementById(place);
-    var img = document.createElement("img");
-    img.src = "./content/images/" + getName(color, name) + ".png";
+    var img = document.createElement(Img);
+    img.src = Location + getName(color, name) + Png;
     startPiece.append(img);
-    for( columnIndex = columnCoordinate - 1, rowIndex = rowCoordinate - 1; columnIndex > 0 && rowIndex > 0; columnIndex--, rowIndex--){
-        if(type == Horizontal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate - 1, rowIndex = rowCoordinate - 1; columnIndex > 0 && rowIndex > 0; columnIndex--, rowIndex--){
+        if (type == Horizontal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
-    for( columnIndex = columnCoordinate + 1, rowIndex = rowCoordinate - 1; columnIndex < 9 && rowIndex > 0; columnIndex++, rowIndex--){
-        if(type == Horizontal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate + 1, rowIndex = rowCoordinate - 1; columnIndex < 9 && rowIndex > 0; columnIndex++, rowIndex--){
+        if (type == Horizontal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
-    for( columnIndex = columnCoordinate + 1, rowIndex = rowCoordinate + 1; columnIndex < 9 && rowIndex < 9; columnIndex++, rowIndex++){
-        if(type == Horizontal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate + 1, rowIndex = rowCoordinate + 1; columnIndex < 9 && rowIndex < 9; columnIndex++, rowIndex++){
+        if (type == Horizontal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
-    for( columnIndex = columnCoordinate - 1, rowIndex = rowCoordinate + 1; columnIndex > 0 && rowIndex < 9; columnIndex--, rowIndex++){
-        if(type == Horizontal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate - 1, rowIndex = rowCoordinate + 1; columnIndex > 0 && rowIndex < 9; columnIndex--, rowIndex++){
+        if (type == Horizontal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
-    for( columnIndex = columnCoordinate - 1 , rowIndex = rowCoordinate; columnIndex > 0; columnIndex--){
-        if(type == Diagonal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate - 1 , rowIndex = rowCoordinate; columnIndex > 0; columnIndex--){
+        if (type == Diagonal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
-    for( columnIndex = columnCoordinate + 1 , rowIndex = rowCoordinate; columnIndex < 9; columnIndex++){
-        if(type == Diagonal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate + 1 , rowIndex = rowCoordinate; columnIndex < 9; columnIndex++){
+        if (type == Diagonal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
-    for( columnIndex = columnCoordinate , rowIndex = rowCoordinate + 1; rowIndex < 9; rowIndex++){
-        if(type == Diagonal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate , rowIndex = rowCoordinate + 1; rowIndex < 9; rowIndex++){
+        if (type == Diagonal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
-    for( columnIndex = columnCoordinate , rowIndex = rowCoordinate - 1; rowIndex > 0; rowIndex--){
-        if(type == Diagonal) break;
-        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + "<br />";
+    for (columnIndex = columnCoordinate , rowIndex = rowCoordinate - 1; rowIndex > 0; rowIndex--){
+        if (type == Diagonal){ 
+            break;
+        }
+        insert.innerHTML += coordinate(getLetter(columnIndex), rowIndex) + Br;
         changeColor(getLetter(columnIndex), rowIndex, Green);
-        if(name == king) break;
+        if (name == pieceTypeKing){ 
+            break;
+        }
     }
 }
 
@@ -135,22 +182,22 @@ class Pawn extends Piece {
     getCoordinate(){
         reset();
         changeColor(this.letter, this.number, Yellow)
-        var place = this.letter + "_" + this.number;
+        var place = this.letter + Underscore + this.number;
         var startPiece = document.getElementById(place);
-        var img = document.createElement("img");
-        img.src = "./content/images/" + getName(this.color, this.name) + ".png";
+        var img = document.createElement(Img);
+        img.src = Location + getName(this.color, this.name) + Png;
         startPiece.append(img);
         var numberX = getNumber(this.letter);
         var numberY = this.number;
         pawnType(this.color);
         numberY += moves[0];
-        insert.innerHTML = getName(this.color, this.name) + "<br />" + "posible moves" + "<br />";
-        insert.innerHTML += coordinate(this.letter, numberY) + "<br />";
+        insert.innerHTML = getName(this.color, this.name) + Br + PossibleMove + Br;
+        insert.innerHTML += coordinate(this.letter, numberY) + Br;
         changeColor(this.letter, numberY, Green);
         numberY = this.number;
         if (this.number == moves[2]){
             numberY += moves[1];
-            insert.innerHTML += this.letter + "_" + numberY + "<br />";
+            insert.innerHTML += this.letter + Underscore + numberY + Br;
             changeColor(this.letter, numberY, Green);
         }
     }
@@ -164,20 +211,20 @@ class Knight extends Piece {
     getCoordinate(){
         reset();
         changeColor(this.letter, this.number, Yellow)
-        var place = this.letter + "_" + this.number;
+        var place = this.letter + Underscore + this.number;
         var startPiece = document.getElementById(place);
-        var img = document.createElement("img");
-        img.src = "./content/images/" + getName(this.color, this.name) + ".png";
+        var img = document.createElement(Img);
+        img.src = Location + getName(this.color, this.name) + Png;
         startPiece.append(img);
         var numberX = getNumber(this.letter);
         var numberY = this.number;
-        insert.innerHTML = getName(this.color, this.name) + "<br />" + "posible moves" + "<br />";
+        insert.innerHTML = getName(this.color, this.name) + Br + PossibleMove + Br;
         for (var Index = 0; Index < 8; Index++){
             if (numberX + KnightMoves[Index][0] > 0 && numberX + KnightMoves[Index][0] < 9 && 
                 numberY + KnightMoves[Index][1] > 0 && numberY + KnightMoves[Index][1] < 9){
                     numberX += KnightMoves[Index][0];
                     numberY += KnightMoves[Index][1];
-                    insert.innerHTML += coordinate(getLetter(numberX), numberY) + "<br />";
+                    insert.innerHTML += coordinate(getLetter(numberX), numberY) + Br;
                     changeColor(getLetter(numberX), numberY, Green);
                     numberX = getNumber(this.letter);
                     numberY = this.number;
@@ -243,21 +290,21 @@ class Rook extends Piece {
 const arr_1 = ["king", "queen", "rook", "bishop", "knight", "pawn"];
 const arr_2 = [King, Queen, Rook, Bishop, Knight, Pawn];
 $(function() {
-    $("body").keydown(function(e) {
-        var keyCode = e.keyCode || e.which;
+    $(Body).keydown(function(key) {
+        var keyCode = key.keyCode || key.which;
         if (keyCode == 13) {
             input();      
         }  
     });
-    $("#enter").click(function(){
+    $(Enter).click(function(){
         input();
     });
 });
 function input(){       
-    var value = $("#piece").val();
-    var color = value.split(",")[0];
-    var name = value.split(",")[1];
-    var coordinate = value.split(",")[2];
+    var value = $(PieceId).val();
+    var color = value.split(Comma)[0];
+    var name = value.split(Comma)[1];
+    var coordinate = value.split(Comma)[2];
     if (document.getElementById(previousPiece) !== null && document.getElementById(previousPiece).firstChild !== null){
         var piece = document.getElementById(previousPiece).firstChild;
         piece.remove();
