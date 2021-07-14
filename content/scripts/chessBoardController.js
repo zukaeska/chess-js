73b4bb30dbe28f6a60252a07383fba84c5df7a82
zukaeska <esakiazuka@gmail.com>
2021-07-14 21:07:40 +0400

    change

--- content/scripts/chessBoardController.js
+++ content/scripts/chessBoardController.js
@@ -1,8 +1,13 @@
 var checker = 0;
 var colorArray = [];
-
+var turnIndex = 0;
 
 function getMoves(object) {
+
+    if(turn() != object.color){
+        return;
+     }
+
     reset();
 
     colorArray = object.getMove();
@@ -10,3 +15,8 @@ function getMoves(object) {
     changeColor(colorArray);
 }
 
+
+function turn(){
+    if (turnIndex % 2 == 0) {return White;}
+    else {return Black} 
+}
